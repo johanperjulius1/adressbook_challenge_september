c0e09e3338ad970d73c3e9151c626c5123dbb3d8
Johan Berglund <johan.per.julius.berglund@gmail.com>
2020-09-02 15:42:37 +0200

    I am adding tests for the input form. They are at first of course failing as I don't have any implementation code. I am adding implementation code and the tests are now passing.

--- cypress/integration/userCanCreateAContact.feature.js
+++ cypress/integration/userCanCreateAContact.feature.js
@@ -4,7 +4,8 @@ describe (' user can create a contact',() => {
     cy.get('#add-contact').click()
     cy.get('#name').type('Johan')
     cy.get('#email').type('Johan@hotmail.com')
-    cy.get('#company').type('Railraiders.io')
+    cy.get('#companyname').type('Railraiders.io')
+    cy.get('#phone').type('0724415305')
   })
 })
 
