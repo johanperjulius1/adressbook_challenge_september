0d3edfe4942166be22c083dd4d0c8519155b98a3
Johan Berglund <johan.per.julius.berglund@gmail.com>
2020-09-02 18:39:48 +0200

    I am adding the final tests that see if user input is stored. The test fails as I have yet to reach the step of the implementation code.

--- cypress/integration/userCanCreateAContact.feature.js
+++ cypress/integration/userCanCreateAContact.feature.js
@@ -6,6 +6,15 @@ describe (' user can create a contact',() => {
     cy.get('#email').type('Johan@hotmail.com')
     cy.get('#companyname').type('Railraiders.io')
     cy.get('#phone').type('0724415305')
+    cy.get('#submit').click()
+  })
+
+  it ('displays the name of a new contact', () => {
+    cy.get('#contact-list').should('contain', 'Johan')
+  })
+
+  it('displays the phone number of the new contact', () => {
+          cy.get('#contact-list').should('contain', '0724415305')
   })
 })
 
