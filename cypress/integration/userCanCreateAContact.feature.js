520bb3722917bdb81279a2f455be7e8541d2a0ff
Johan Berglund <johan.per.julius.berglund@gmail.com>
2020-09-02 15:18:31 +0200

    I am adding a number of tests that check for elements that will be in the form where information about the contact is added.

--- cypress/integration/userCanCreateAContact.feature.js
+++ cypress/integration/userCanCreateAContact.feature.js
@@ -2,6 +2,9 @@ describe (' user can create a contact',() => {
   it('test', () => {
     cy.visit('http://localhost:3001')
     cy.get('#add-contact').click()
-
+    cy.get('#name').type('Johan')
+    cy.get('#email').type('Johan@hotmail.com')
+    cy.get('#company').type('Railraiders.io')
   })
 })
+
